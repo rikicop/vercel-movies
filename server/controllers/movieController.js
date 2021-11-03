f006b81351436dc883184f1225d0a2320b73c918
rikicop <ruperto1@protonmail.com>
2021-11-02 22:39:03 -0400

    db test

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,3 +1,14 @@
+require("../models/db");
+const Movie = require("../models/movie");
+
+/* /api/movies/ */
+/* GET All Movies */
+
 exports.listMovies = async (req, res) => {
-  res.send("Hello Ricardo");
+  try {
+    const movies = await Movie.find({});
+    res.json(movies);
+  } catch (err) {
+    res.status(400).json({ message: err });
+  }
 };
