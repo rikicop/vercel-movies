124d209a4b655c74be379941b5bb6d17b4498872
rikicop <ruperto1@protonmail.com>
2021-11-02 20:00:32 -0400

    second

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get("/", (req, res) => res.send("Home Page Route"));
 
 app.get("/about", (req, res) => res.send("Acerca de que!!"));
 
-app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+app.get("/portfolio", (req, res) =>
+  res.send("De que portafolio estás hablando!!")
+);
 
 app.get("/contact", (req, res) => res.send("Contact Page Route"));
 
